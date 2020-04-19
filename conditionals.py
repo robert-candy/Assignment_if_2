07512fb49eb4e55da168c88d8f99aacb0d25195e
Robert <candyrobertt@gmail.com>
2020-04-20 01:37:22 +0300

    first commit

--- conditionals.py
+++ conditionals.py
@@ -1,1 +1,18 @@
-# This file has been created on gitHub
+
+user_input = input("Are you a cigarette addict older than 75 years old? [yes/no]")
+age = user_input == "yes"
+
+user_input = input("Do you have a severe chronic disease? [yes/no]")
+chronic = user_input == "yes"
+
+user_input = input("Is your immune system too weak? [yes/no]")
+immune = (user_input == "yes")
+
+risk = age and chronic and immune
+
+
+if risk:
+    print("You are in risky group")
+else:
+    print("You are not in risky group")
+
